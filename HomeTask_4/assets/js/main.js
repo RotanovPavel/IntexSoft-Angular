c402f1b90678476850b8ba451608ad36f17d3807
Rotanov_Pavel <pavel.rotanov@dynevo.org>
2020-02-22 15:37:26 +0300

    HT4 with http-server

---
+++ HomeTask_4/assets/js/main.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+axios.get('https://dog.ceo/api/breeds/image/random').then((response) => {
+    const imageElement = document.querySelector('img');
+    imageElement.src = response.data.message;
+});
